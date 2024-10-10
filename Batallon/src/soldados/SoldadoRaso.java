0d22347e4b3eaf38c07832683abe7796e2dbce32
Svaldes77 <samuel.valdes@correounivalle.edu.co>
2024-10-10 11:56:36 -0500

    añado comentarios a soldadoraso y a teniente

--- Batallon/src/soldados/SoldadoRaso.java
+++ Batallon/src/soldados/SoldadoRaso.java
@@ -2,17 +2,21 @@ package soldados;
 import rangos.Nivel_militar;
 //import rangos.Rango;
 import misiones.OperacionesMilitares;
+//importamos los paquetes necesarios
 
+//Clase SoldadoRaso que hereda de la clase Soldado e implementa la interfaz OperacionesMilitares
 public class SoldadoRaso extends Soldado implements OperacionesMilitares {
 
+    //Constructor de la clase SoldadoRaso que recibe los parametros nivel, nombre, id y rango
     public SoldadoRaso(int nivel, String nombre, String id, Nivel_militar rango) {
         super(nivel, nombre, id,rango);
         this.nivel = 1; //nivel de soldado raso 
         //this.rango =  "Soldado Raso";
-        this.rango = Nivel_militar.SOLDADO_RASO;
+        this.rango = Nivel_militar.SOLDADO_RASO; //Inicialización de atributos propios de la clase SoldadoRaso
 
     }
 
+    //Sobre escritura de metodos de la interfaz OperacionesMilitares
     @Override 
     public void asignarMision(String mision){
         System.out.println("Soldado Raso asignado a la misión: " + mision);
@@ -20,13 +24,14 @@ public class SoldadoRaso extends Soldado implements OperacionesMilitares {
     };
 
     public void reportarEstado(){ 
-        System.out.println(Nivel_militar.CORONEL+"ESTOY VIGIA EN LA TORRETA");     
+        System.out.println(Nivel_militar.CORONEL+"ESTOY VIGIA EN LA TORRETA"); //Mensaje que se imprime en consola
      };
 
-     //Sobre escrit
+
+    //Sobre escritura de metodos de la clase padre
     @Override
     public void realizarAccion() {
-        System.out.println("Soy un: "+ Nivel_militar.SOLDADO_RASO + "estoy en la trinchera");
+        System.out.println("Soy un: "+ Nivel_militar.SOLDADO_RASO + "estoy en la trinchera");//Mensaje que se imprime en consola
     }
 
 }
--- Batallon/src/soldados/Teniente.java
+++ Batallon/src/soldados/Teniente.java
@@ -2,26 +2,30 @@ package soldados;
 import rangos.Nivel_militar;
 // import rangos.Rango;
 // import misiones.OperacionesMilitares;
+// importamos los packages necesarios
 
+//Clase Teniente que hereda de la clase Soldado
 public class Teniente extends Soldado {
-    private String unidad; 
+    private String unidad; //Atributo de la clase Teniente
 
+    //Constructor de la clase Teniente que recibe los parametros nivel, nombre, id, rango y unidad
     public Teniente(int nivel, String nombre, String id, Nivel_militar rango , String unidad) {
-        super(nivel, nombre, id, rango);
+        super(nivel, nombre, id, rango);//Llamada al constructor de la clase padre
         this.rango = Nivel_militar.TENIENTE;
         this.unidad = unidad; 
     }
 
+    //Metodos Get y Set
     public String getUnidad() {
         return unidad;
     }
-
     public void setUnidad(String unidad) {
         this.unidad = unidad;
     } 
 
+    //Metodo que se sobre escribe de la clase padre
     @Override
     public void realizarAccion() {
-        System.out.println("Soy el "+ Nivel_militar.TENIENTE + " y estoy Supervisando la trinchera");  
+        System.out.println("Soy el "+ Nivel_militar.TENIENTE + " y estoy Supervisando la trinchera"); //Mensaje que se imprime en consola
     }
 }
