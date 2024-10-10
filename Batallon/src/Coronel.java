ef3d622bdb7cf4b395b1f7c8195b8f3860aca091
santiago.aguado@correounivalle.edu.co <santiago.aguado@correounivalle.edu.co>
2024-10-09 20:28:17 -0500

    Interfaces para cada clase

--- Batallon/src/Coronel.java
+++ Batallon/src/Coronel.java
@@ -1,18 +1,31 @@
-public class Coronel extends Soldado {
+public class Coronel extends Soldado implements OperacionesMilitares{
+    
+    //Atributos
+
     private String estrategia; 
     
+    //Constructor Coronel
     public Coronel(int nivel, String nombre, String id, Nivel_militar rango, String estrategia) {
         super(nivel, nombre, id,rango); 
         this.estrategia = estrategia; 
     }
-    
 
+    //Sobre escritura de metodos
     @Override
     public void realizarAccion() {
         System.out.println("Soy un coronel y estoy Preparando la estrategia de ataque"); 
     }
 
+    @Override
+    public void reportarEstado() {
+        System.out.println("Soy un coronel y estoy reportando el estado de la tropa"); 
+    }
+    
+    @Override
+    public void asignarMision(String mision) {
+        System.out.println("Soy un coronel y estoy asignando la mision de: "+ mision);
 
+    }
     //Metodos Get y Set
 
     public String getEstrategia() {
