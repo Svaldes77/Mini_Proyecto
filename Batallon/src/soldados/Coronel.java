700b6bd8c02a4c50f6df9c28c7a899ce2c4cbc45
santiago.aguado@correounivalle.edu.co <santiago.aguado@correounivalle.edu.co>
2024-10-11 22:32:25 -0500

    Comentarios en el script Coronel, aplicando Google Java Style y tambien corrigiendo sintaxis

--- Batallon/src/soldados/Coronel.java
+++ Batallon/src/soldados/Coronel.java
@@ -1,4 +1,5 @@
 package soldados; //Paquete de la Clase
+
 import rangos.Nivel_militar;
 //import rangos.Rango;
 import misiones.OperacionesMilitares;
@@ -7,36 +8,40 @@ import misiones.OperacionesMilitares;
 //Clase Coronel que hereda de la clase Soldado e implementa la interfaz OperacionesMilitares
 public class Coronel extends Soldado implements OperacionesMilitares{
 
-    //Atributos
+    // Atributo que almacena la estrategia del coronel
     private String estrategia; 
     
     //Constructor Coronel
     public Coronel(int nivel, String nombre, String id, Nivel_militar rango, String estrategia) {
-        super(nivel, nombre, id,rango); 
+        super(nivel, nombre, id,rango); // Llama al constructor de la clase base Soldado 
         this.estrategia = estrategia; //Inicialización de atributos propios de la clase Coronel
     }
 
-    //Sobre escritura de metodos
+    // Sobrescritura del método realizarAccion de la clase Soldado
     @Override
     public void realizarAccion() {
         System.out.println("Soy un coronel y estoy Preparando la estrategia de ataque"); 
     }
 
+    // Sobrescritura del método reportarEstado de la interfaz OperacionesMilitares
     @Override
     public void reportarEstado() {
         System.out.println("Soy un coronel y estoy reportando el estado de la tropa"); 
     }
     
+    // Sobrescritura del método asignarMision de la interfaz OperacionesMilitares
     @Override
     public void asignarMision(String mision) {
         System.out.println("Soy un coronel y estoy asignando a la mision de: "+ mision);
         // + mision es para concatenar el valor de la variable mision que le otorgamos en el app.java
-
     }
-    //Metodos Get y Set
+    
+    // Método getter para obtener la estrategia
     public String getEstrategia() {
         return estrategia;
     }
+
+    // Método setter para establecer la estrategia
     public void setEstrategia(String estrategia) {
         this.estrategia = estrategia;
     }
