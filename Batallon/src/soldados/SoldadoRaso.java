467f25e13491f4f1cea89a69c7369317e0b3730d
santiago.aguado@correounivalle.edu.co <santiago.aguado@correounivalle.edu.co>
2024-10-11 22:49:40 -0500

    Correccion de sintaxis (espacios innecesarios)

--- Batallon/src/soldados/SoldadoRaso.java
+++ Batallon/src/soldados/SoldadoRaso.java
@@ -1,4 +1,5 @@
 package soldados;
+
 import rangos.Nivel_militar;
 //import rangos.Rango;
 import misiones.OperacionesMilitares;
@@ -13,20 +14,17 @@ public class SoldadoRaso extends Soldado implements OperacionesMilitares {
         this.nivel = 1; //nivel de soldado raso 
         //this.rango =  "Soldado Raso";
         this.rango = Nivel_militar.SOLDADO_RASO; //Inicialización de atributos propios de la clase SoldadoRaso
-
     }
 
     //Sobre escritura de metodos de la interfaz OperacionesMilitares
     @Override 
     public void asignarMision(String mision){
         System.out.println("Soldado Raso asignado a la misión: " + mision);
-
     };
 
     public void reportarEstado(){ 
         System.out.println(Nivel_militar.CORONEL+"ESTOY VIGIA EN LA TORRETA"); //Mensaje que se imprime en consola
      };
-
 
     //Sobre escritura de metodos de la clase padre
     @Override
