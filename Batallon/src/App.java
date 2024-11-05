6382d395740e1cfe07232473c9de6a3c44fea461
Svaldes77 <samuel.valdes@correounivalle.edu.co>
2024-11-04 21:52:19 -0500

    documentacion del codigo del anterior commit

--- Batallon/src/App.java
+++ Batallon/src/App.java
@@ -76,7 +76,7 @@ public class App {
 
         switch (opcion) {
             case 1:
-                listaSoldados.add(SoldadoRaso.crearSoldadoRaso(scanner));
+                listaSoldados.add(SoldadoRaso.crearSoldadoRaso(scanner)); // agrega el soldado creado a la lista
                 break;
             case 2:
                 listaSoldados.add(Teniente.crearTeniente(scanner));
@@ -88,7 +88,7 @@ public class App {
                 //listaSoldados.add(crearcapitan(scanner));
                 break;
             case 5:
-                Soldado.mostrarInformacion(listaSoldados);
+                Soldado.mostrarInformacion(listaSoldados); // Muestra la información de todos los soldados
                 break;
             case 6:
                 System.out.println("Saliendo...");
@@ -97,10 +97,10 @@ public class App {
             default:
                 System.out.println("Opción no válida. Intente de nuevo.");
             }
-        } catch (InputMismatchException e) {
+        } catch (InputMismatchException e) { //InputMismatchException e lo utilizamos para que el programa no se caiga si el usuario ingresa un valor que no es un numero
                 System.out.println("Entrada no válida. Por favor, ingrese un número.");
                 scanner.nextLine(); // Consumir la entrada no válida
-            } catch (IllegalArgumentException e) {
+            } catch (IllegalArgumentException e) { //IllegalArgumentException e lo utilizamos para que el programa no se caiga si el usuario ingresa un rango que no es válido
                 System.out.println("Rango no válido. Por favor, ingrese un rango válido.");
             } catch (Exception e) {
                 System.out.println("Ocurrió un error: " + e.getMessage());
