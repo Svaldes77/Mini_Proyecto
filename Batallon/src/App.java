ddd8033fae9ead0598837b83faafc512c2fdfa3b
Svaldes77 <samuel.valdes@correounivalle.edu.co>
2024-11-27 22:22:38 -0500

    traer deploy a mi rama

--- Batallon/src/App.java
+++ Batallon/src/App.java
@@ -819,12 +819,8 @@ import soldados.Teniente;
                         jListSoldados.clearSelection(); 
                         buttonGroupAcciones.clearSelection(); 
                 }                                             
-<<<<<<< HEAD
-                    
-=======
 
                 
->>>>>>> origin/bypipe
             }
                           
             
